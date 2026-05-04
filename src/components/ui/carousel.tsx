13df88044429e2944227b94687afd0e241fb0ffa
ikyoh <myikyoh@gmail.com>
2026-05-04 08:58:55 +0200

    Update carousel.tsx

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
 import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react"
 import { ArrowLeft, ArrowRight } from "lucide-react"
+import * as React from "react"
 
-import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
@@ -118,7 +118,7 @@ function Carousel({
     >
       <div
         onKeyDownCapture={handleKeyDown}
-        className={cn("relative", className)}
+        className={cn("relative max-w-full!", className)}
         role="region"
         aria-roledescription="carousel"
         data-slot="carousel"
@@ -230,10 +230,8 @@ function CarouselNext({
 }
 
 export {
-  type CarouselApi,
   Carousel,
   CarouselContent,
-  CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
+  CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi
 }
+
