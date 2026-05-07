de671bc5a3a29ca67f945b2c80ff79f2b36de922
ikyoh <myikyoh@gmail.com>
2026-05-07 13:56:05 +0200

    Font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,11 @@
 
 import { Head, Html, Main, NextScript } from "next/document";
-import { Montserrat } from "next/font/google";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-sans",
-});
-
 
 export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head />
-      <body className={`${montserrat.className} ${montserrat.variable} bg-background text-foreground`}>
+      <body className="bg-background text-foreground">
           <Main />
           <NextScript />
       </body>
