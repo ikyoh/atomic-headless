a4918c5099af8f1e94d9cd3dec22fad36be13cde
ikyoh <myikyoh@gmail.com>
2026-05-11 14:14:55 +0200

    Update HeroYoutube.jsx

--- src/components/HeroYoutube.jsx
+++ src/components/HeroYoutube.jsx
@@ -8,8 +8,8 @@ export default function HeroYoutube({ youtubeID = "YuN82rArMqM", title, subtitle
             <iframe
                 className="pointer-events-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-clip
                         w-screen h-[56.25vw] min-h-[calc(100vh-90px)] min-w-[177.78dvh]"
-                src={`https://www.youtube.com/embed/${youtubeID}?autoplay=1&loop=1&playlist=${youtubeID}&mute=1&playsinline=1&iv_load_policy=3&controls=0&disablekb=1&fs=0`}
-                title="YouTube video player"
+                src={`https://www.youtube.com/embed/${youtubeID}?autoplay=1&loop=1&playlist=${youtubeID}&mute=1&playsinline=1&iv_load_policy=3&controls=0&disablekb=1&fs=0&modestbranding=1`}
+                title="Atomic Néon YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
             ></iframe>
