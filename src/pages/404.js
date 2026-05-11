52a479ab45a6442ed75fa9d60bd4de44b52a8079
ikyoh <myikyoh@gmail.com>
2026-05-11 14:10:42 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,32 +9,34 @@ import { print } from "graphql";
 import "../../faust.config.js";
 
 const PAGE_404_QUERY = gql`
-	${blocks.CoreParagraph.fragments.entry}
-	${blocks.CoreColumns.fragments.entry}
-	${blocks.CoreColumn.fragments.entry}
-	${blocks.CoreButtons.fragments.entry}
-	${blocks.CoreButton.fragments.entry}
-	${blocks.CoreQuote.fragments.entry}
-	${blocks.CoreImage.fragments.entry}
-	${blocks.CoreSeparator.fragments.entry}
-	${blocks.CoreList.fragments.entry}
-	${blocks.CoreHeading.fragments.entry}
-	${blocks.OtherFooterCta.fragments.entry}
-	${blocks.OtherProductsCategoriesCards.fragments.entry}
-	${blocks.AcfCardHeader.fragments.entry}
-	${blocks.AcfCardPicture.fragments.entry}
-	${blocks.AcfCardLink.fragments.entry}
-	${blocks.AcfItemPicture.fragments.entry}
-	${blocks.AcfTeam.fragments.entry}
-	${blocks.AcfCarousel.fragments.entry}
-	${blocks.AcfCarouselRoundedCards.fragments.entry}
-	${blocks.AcfCarouselLabelCards.fragments.entry}
-	${blocks.AcfKnowHowCard.fragments.entry}
-	${blocks.AcfLogos.fragments.entry}
-	${blocks.AcfYoutube.fragments.entry}
-	${blocks.AcfTimeline.fragments.entry}
-	${blocks.AcfAccordionPicture.fragments.entry}
-	${blocks.CoreGroup.fragments.entry}
+
+  ${blocks.CoreParagraph.fragments.entry}
+  ${blocks.CoreColumns.fragments.entry}
+  ${blocks.CoreColumn.fragments.entry}
+  ${blocks.CoreButtons.fragments.entry}
+  ${blocks.CoreButton.fragments.entry}
+  ${blocks.CoreQuote.fragments.entry}
+  ${blocks.CoreImage.fragments.entry}
+  ${blocks.CoreSeparator.fragments.entry}
+  ${blocks.CoreList.fragments.entry}
+  ${blocks.CoreHeading.fragments.entry}
+  ${blocks.OtherFooterCta.fragments.entry}
+  ${blocks.OtherProductsCategoriesCards.fragments.entry}
+  ${blocks.AcfCardHeader.fragments.entry}
+  ${blocks.AcfCardPicture.fragments.entry}
+  ${blocks.AcfCardLink.fragments.entry}
+  ${blocks.AcfItemPicture.fragments.entry}
+  ${blocks.AcfTeam.fragments.entry}
+  ${blocks.AcfCarousel.fragments.entry}
+  ${blocks.AcfCarouselRoundedCards.fragments.entry}
+  ${blocks.AcfCarouselLabelCards.fragments.entry}
+  ${blocks.AcfKnowHowCard.fragments.entry}
+  ${blocks.AcfLogos.fragments.entry}
+  ${blocks.AcfYoutube.fragments.entry}
+  ${blocks.AcfTimeline.fragments.entry}
+  ${blocks.AcfAccordionPicture.fragments.entry}
+  ${blocks.CoreGroup.fragments.entry}
+
 
 	query Get404Page($uri: String!) {
 		pageBy(uri: $uri) {
@@ -47,13 +49,40 @@ const PAGE_404_QUERY = gql`
 					mediaItemUrl
 				}
 			}
-			editorBlocks {
-				name
-				__typename
-				renderedHtml
-				id: clientId
-				parentId: parentClientId
-			}
+      editorBlocks {
+        name
+        __typename
+        renderedHtml
+        id: clientId
+        parentId: parentClientId
+        ...${blocks.CoreParagraph.fragments.key}
+        ...${blocks.CoreColumns.fragments.key}
+        ...${blocks.CoreColumn.fragments.key}
+        ...${blocks.CoreButtons.fragments.key}
+        ...${blocks.CoreButton.fragments.key}
+        ...${blocks.CoreQuote.fragments.key}
+        ...${blocks.CoreImage.fragments.key}
+        ...${blocks.CoreSeparator.fragments.key}
+        ...${blocks.CoreList.fragments.key}
+        ...${blocks.CoreHeading.fragments.key}
+        ...${blocks.OtherFooterCta.fragments.key}
+        ...${blocks.OtherProductsCategoriesCards.fragments.key}
+        ...${blocks.AcfCardPicture.fragments.key}
+        ...${blocks.AcfCardLink.fragments.key}
+        ...${blocks.AcfCardHeader.fragments.key}
+        ...${blocks.AcfItemPicture.fragments.key}
+        ...${blocks.AcfTeam.fragments.key}
+        ...${blocks.AcfCarousel.fragments.key}
+        ...${blocks.AcfCarouselRoundedCards.fragments.key}
+        ...${blocks.AcfCarouselLabelCards.fragments.key}
+        ...${blocks.AcfKnowHowCard.fragments.key}
+        ...${blocks.AcfLogos.fragments.key}
+        ...${blocks.AcfYoutube.fragments.key}
+        ...${blocks.AcfAccordionPicture.fragments.key}
+        ...${blocks.AcfTimeline.fragments.key}
+        ...${blocks.CoreGroup.fragments.key}
+
+      }
 		}
 
 		footer: templatePart(id: "footer", idType: SLUG) {
@@ -97,23 +126,17 @@ const PAGE_404_QUERY = gql`
 `;
 
 export default function Custom404({ data }) {
+
+	console.log("404 page data", data);
 	const page = data?.page;
 	const footerBlocks = data?.footer?.editorBlocks;
 
-	if (!page) {
-		return (
-			<Layout footerBlocks={footerBlocks}>
-				<ProductsBigMenu />
-				<main id="content">
-					<h1>Page not found</h1>
-				</main>
-			</Layout>
-		);
-	}
 
 	const blockList = flatListToHierarchical(page.editorBlocks ?? [], {
 		childrenKey: "innerBlocks",
 	});
+
+console.log('blockList', blockList)
 
 	return (
 		<Layout footerBlocks={footerBlocks}>
@@ -123,7 +146,7 @@ export default function Custom404({ data }) {
 				title={page.title}
 				subtitle={page.subtitle?.subtitle}
 			/>
-			<main id="content">
+			<main id="content">fsdfsd
 				<WordPressBlocksViewer blocks={blockList} />
 			</main>
 		</Layout>
