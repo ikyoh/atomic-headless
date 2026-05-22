d574b65ca5ddce0455e1ed28a579211a7011510d
ikyoh <myikyoh@gmail.com>
2026-05-22 14:59:39 +0200

    sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -4,7 +4,7 @@ export default function Sitemap() {}
 
 export function getServerSideProps(ctx) {
   return getSitemapProps(ctx, {
-    // sitemapIndexPath: '/sitemap_index.xml', // Update the sitemap path if a WordPress plugin is used that changes the default path.
+    itemapIndexPath: '/sitemap_index.xml', // Update the sitemap path if a WordPress plugin is used that changes the default path.
     frontendUrl: process.env.NEXT_PUBLIC_FRONTEND_URL,
     sitemapPathsToIgnore: ["/wp-sitemap-users-*","/wp-sitemap-taxonomies-*","/wp-sitemap-posts-wp_template_*"],
   });
